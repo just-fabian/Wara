729977a882424c147156df23e316054b2b2d8f7e
just-fabian <fabian130404@gmail.com>
2024-10-05 10:47:17 -0400

    feat: add theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import { Container } from "@mui/material";
+import { Container, ThemeProvider } from "@mui/material";
+import theme from "./utils/theme";
 
 function App() {
-
-  return <Container></Container>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Container></Container>
+    </ThemeProvider>
+  );
 }
 
 export default App;
